1b37f7e93df49aeca1f9708f3872691a9121b396
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-02 12:06:53 +0100

    creata struttura in main.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const root = new Vue({
+    el: "#root",
+    data: {
+        listaTask: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato"
+        ],
+    }
+})
